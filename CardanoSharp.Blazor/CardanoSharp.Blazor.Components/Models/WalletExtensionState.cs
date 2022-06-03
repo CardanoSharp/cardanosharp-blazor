6a353b415484e6e999bd20ff9735bd2b5baea5d6
Paul Cowan <paul.cowan@gmail.com>
2022-06-03 23:06:40 +0200

    Did card layout for connected wallet and added balance basics to connected wallet display

--- CardanoSharp.Blazor/CardanoSharp.Blazor.Components/Models/WalletExtensionState.cs
+++ CardanoSharp.Blazor/CardanoSharp.Blazor.Components/Models/WalletExtensionState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CardanoSharp.Wallet.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,36 @@ namespace CardanoSharp.Blazor.Components.Models
         public bool Installed { get; set; }
         public bool Enabled { get; set; }
         public bool Connected { get; set; }
+        public ulong Balance { get; set; }
+        public string BalanceAda
+        {
+            get
+            {
+                return (Balance / 1000000).ToString();
+            }
+        }
+        public string BalanceRemainder
+        {
+            get
+            {
+                return (Balance - (Balance / 1000000)).ToString("X6");
+            }
+        }
+        public NetworkId Network { get; set; }
+        public string CoinCurrency
+        {
+            get
+            {
+                if (Network == NetworkId.Mainnet)
+                {
+                    return "₳";
+                }
+                return "t₳";
+            }
+        }
+
+        public ulong TokenPreservation { get; set; }
+        public int TokenCount { get; set; }
 
         public WalletExtensionState(WalletExtension copy)
             : base(copy)
