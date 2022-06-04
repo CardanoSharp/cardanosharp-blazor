e347a9152b6ac4562bac07e3add34c793cbdac79
Paul Cowan <paul.cowan@gmail.com>
2022-06-04 14:45:40 +0200

    Tweaked layout and formatting for balances

--- CardanoSharp.Blazor/CardanoSharp.Blazor.Components/Models/WalletExtensionState.cs
+++ CardanoSharp.Blazor/CardanoSharp.Blazor.Components/Models/WalletExtensionState.cs
@@ -1,6 +1,7 @@
 ﻿using CardanoSharp.Wallet.Enums;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,14 +18,15 @@ namespace CardanoSharp.Blazor.Components.Models
         {
             get
             {
-                return (Balance / 1000000).ToString();
+                var temp = (Balance / 1000000).ToString("N", CultureInfo.CreateSpecificCulture("en-US"));
+                return temp.Substring(0, temp.IndexOf('.'));
             }
         }
         public string BalanceRemainder
         {
             get
             {
-                return (Balance - (Balance / 1000000)).ToString("X6");
+                return (Balance % 1000000).ToString("D6");
             }
         }
         public NetworkId Network { get; set; }
@@ -41,6 +43,21 @@ namespace CardanoSharp.Blazor.Components.Models
         }
 
         public ulong TokenPreservation { get; set; }
+        public string TokenPreservationAda
+        {
+            get
+            {
+                var temp = (TokenPreservation / 1000000).ToString("N", CultureInfo.CreateSpecificCulture("en-US"));
+                return temp.Substring(0, temp.IndexOf('.'));
+            }
+        }
+        public string TokenPreservationRemainder
+        {
+            get
+            {
+                return (TokenPreservation % 1000000).ToString("D6");
+            }
+        }
         public int TokenCount { get; set; }
 
         public WalletExtensionState(WalletExtension copy)
