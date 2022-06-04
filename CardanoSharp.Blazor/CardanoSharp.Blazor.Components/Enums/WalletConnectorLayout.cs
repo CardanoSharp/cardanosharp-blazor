203b2e08ab9539fff102885414a7c114255ef154
Paul Cowan <paul.cowan@gmail.com>
2022-06-04 19:19:19 +0200

    Got card layout working

---
+++ CardanoSharp.Blazor/CardanoSharp.Blazor.Components/Enums/WalletConnectorLayout.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardanoSharp.Blazor.Components.Enums
+{
+    public enum WalletConnectorLayout
+    {
+        List = 0,
+        Card = 1,
+    }
+}
