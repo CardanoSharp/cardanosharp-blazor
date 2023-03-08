2aa081d9811143ed945b6fe1df824ba65c40683e
Paul Cowan <paul.cowan@inversion.dev>
2023-03-08 14:13:28 +0200

    changing connected wallet to field and added method to get all wallet extension states

--- CardanoSharp.Blazor/CardanoSharp.Blazor.Components/Models/WalletExtensionState.cs
+++ CardanoSharp.Blazor/CardanoSharp.Blazor.Components/Models/WalletExtensionState.cs
@@ -3,79 +3,94 @@ using System.Globalization;
 
 namespace CardanoSharp.Blazor.Components.Models
 {
-	public class WalletExtensionState : WalletExtension
-	{
-		public bool Installed { get; set; }
+    public class WalletExtensionState : WalletExtension
+    {
+        public bool Installed { get; set; }
 
-		public bool Enabled { get; set; }
+        public bool Enabled { get; set; }
 
-		public bool Connected { get; set; }
+        public bool Connected { get; set; }
 
-		public ulong Balance { get; set; }
+        public string? Version { get; set; }
 
-		public string BalanceAda
-		{
-			get
-			{
-				var temp = (Balance / 1000000).ToString("N", CultureInfo.CreateSpecificCulture("en-US"));
-				return temp.Substring(0, temp.IndexOf('.'));
-			}
-		}
+        public ulong Balance { get; set; }
 
-		public string BalanceRemainder
-		{
-			get
-			{
-				return (Balance % 1000000).ToString("D6");
-			}
-		}
+        public string BalanceAda
+        {
+            get
+            {
+                var temp = (Balance / 1000000).ToString("N", CultureInfo.CreateSpecificCulture("en-US"));
+                return temp.Substring(0, temp.IndexOf('.'));
+            }
+        }
 
-		public NetworkType Network { get; set; }
+        public string BalanceRemainder
+        {
+            get
+            {
+                return (Balance % 1000000).ToString("D6");
+            }
+        }
 
-		public string CoinCurrency
-		{
-			get
-			{
-				if (Network == NetworkType.Mainnet)
-				{
-					return "₳";
-				}
-				else if (Network == NetworkType.Testnet)
-				{
-					return "t₳";
-				}
-				return "";
-			}
-		}
+        public NetworkType Network { get; set; }
 
-		public ulong TokenPreservation { get; set; }
+        public string CoinCurrency
+        {
+            get
+            {
+                if (Network == NetworkType.Mainnet)
+                {
+                    return "₳";
+                }
+                else if (Network == NetworkType.Testnet)
+                {
+                    return "t₳";
+                }
+                return "";
+            }
+        }
 
-		public string TokenPreservationAda
-		{
-			get
-			{
-				var temp = (TokenPreservation / 1000000).ToString("N", CultureInfo.CreateSpecificCulture("en-US"));
-				return temp.Substring(0, temp.IndexOf('.'));
-			}
-		}
+        public ulong TokenPreservation { get; set; }
 
-		public string TokenPreservationRemainder
-		{
-			get
-			{
-				return (TokenPreservation % 1000000).ToString("D6");
-			}
-		}
+        public string TokenPreservationAda
+        {
+            get
+            {
+                var temp = (TokenPreservation / 1000000).ToString("N", CultureInfo.CreateSpecificCulture("en-US"));
+                return temp.Substring(0, temp.IndexOf('.'));
+            }
+        }
 
-		public int TokenCount { get; set; }
+        public string TokenPreservationRemainder
+        {
+            get
+            {
+                return (TokenPreservation % 1000000).ToString("D6");
+            }
+        }
 
-		public WalletExtensionState()
-		{
-		}
+        public int TokenCount { get; set; }
 
-		public WalletExtensionState(WalletExtension copy)
-			: base(copy)
-		{
-		}
-	}
+        public WalletExtensionState()
+        {
+        }
+
+        public WalletExtensionState(WalletExtensionState copy)
+            : base(copy)
+        {
+            Installed = copy.Installed;
+            Enabled = copy.Enabled;
+            Connected = copy.Connected;
+            Version = copy.Version;
+            Balance = copy.Balance;
+            Network = copy.Network;
+            TokenPreservation = copy.TokenPreservation;
+            TokenCount = copy.TokenCount;
+        }
+
+        public WalletExtensionState(WalletExtension copy)
+                : base(copy)
+        {
+        }
+    }
 }
